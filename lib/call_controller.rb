74443c9ebe87f75c9c70155548c1d92a6a395056
David Wilkie <dwilkie@gmail.com>
2015-04-24 17:20:18 +0700

    Remove override for twilio dial

--- lib/call_controller.rb
+++ lib/call_controller.rb
@@ -7,11 +7,4 @@ class CallController < Adhearsion::CallController
     answer
     notify_voice_request_url
   end
-
-  private
-
-  def twilio_dial(node, options = {})
-    play_audio!("https://s3.amazonaws.com/chibimp3/ringback_cambodia.mp3", :renderer => :native)
-    super
-  end
 end
