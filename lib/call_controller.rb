48f4b17a042abbfc8c7a6a50dab4cf9992d12c05
David Wilkie <dwilkie@gmail.com>
2015-05-01 10:05:54 +0700

    Don't override Twilio Dial

--- lib/call_controller.rb
+++ lib/call_controller.rb
@@ -1,17 +1,8 @@
-# encoding: utf-8
-
 class CallController < Adhearsion::CallController
   include Adhearsion::Twilio::ControllerMethods
 
   def run
     answer
     notify_voice_request_url
-  end
-
-  private
-
-  def twilio_dial(node, options = {})
-    play_audio!("https://s3.amazonaws.com/chibimp3/ringback_cambodia.mp3")
-    super
   end
 end
--- spec/lib/call_controller_spec.rb
+++ spec/lib/call_controller_spec.rb
@@ -17,22 +17,5 @@ describe CallController do
       expect(mock_call).to receive(:answer)
       expect_call_status_update { subject.run }
     end
-
-    context "when dialing" do
-      let(:number_to_dial) { "+85512345678" }
-      let(:dial_status) { double(Adhearsion::CallController::DialStatus, :result => :answer ) }
-
-      before do
-        allow(subject).to receive(:dial).and_return(dial_status)
-      end
-
-      it "should first play a ringback" do
-        expect(subject).to receive(:play_audio!).with(
-          "https://s3.amazonaws.com/chibimp3/ringback_cambodia.mp3"
-        )
-        expect(subject).to receive(:dial)
-        expect_call_status_update(:to => number_to_dial, :cassette => :dial) { subject.run }
-      end
-    end
   end
 end
