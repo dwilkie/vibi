13488b52df5ef38ee88ced33e00c175a5c5c1f52
David Wilkie <dwilkie@gmail.com>
2015-05-24 10:49:06 +0700

    More debugging output

--- lib/call_controller.rb
+++ lib/call_controller.rb
@@ -4,6 +4,14 @@ class CallController < Adhearsion::CallController
   def run
     p "_____CALL VARIABLES________"
     p call.variables
+    p "---x_variable_sip_p_asserted_identity---"
+    p call.variables[:x_variable_sip_p_asserted_identity]
+    p "----p_early_media-----"
+    p call.variables[:p_early_media]
+    p "----P-Asserted-Identity-----"
+    p call.variables[:p_asserted_identity]
+    p "----SIP-P-Asserted-Identity-----"
+    p call.variables[:sip_p_asserted_identity]
     notify_voice_request_url
   end
 end
